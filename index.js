c20b1aa73dc985e716efca4895a6728f32dd4d70
Md Sarowar <mdsarowarhang@gmail.com>
2023-05-14 15:14:16 +0300

    all data get on poth

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-require('dotenv').config()
-console.log(process.env.DB_PASS);
+require("dotenv").config();
 
-app.get('/',(req,res)=>{
-    res.send('doctor is running')
-})
+app.get("/", (req, res) => {
+  res.send("doctor is running");
+});
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@ac-o6iroya-shard-00-00.wymoxsw.mongodb.net:27017,ac-o6iroya-shard-00-01.wymoxsw.mongodb.net:27017,ac-o6iroya-shard-00-02.wymoxsw.mongodb.net:27017/?ssl=true&replicaSet=atlas-puyajh-shard-0&authSource=admin&retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,16 +21,36 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const serviceCollection = client.db("carDoctor").collection("Servies");
+    app.get("/services", async (req, res) => {
+      const cursor = serviceCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    
+    app.get('/services/:id',async(req,res)=>{
+       const id = req.params.id;
+       console.log(id);
+       const options = {
+        // Include only the `title` and `imdb` fields in the returned document
+        projection: { title: 1, price: 1, service_id:1 },
+      };
+       const query = {_id: new ObjectId(id)}
+       const result = await serviceCollection.findOne(query,options)
+       res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -39,8 +58,6 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.listen(port,()=>{
-    console.log(`car Doctor is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`car Doctor is running on port ${port}`);
+});
